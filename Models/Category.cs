d06e5d795a4435246cfb029da306dc0104721aff
zinmyoswe <overmidnight12@gmail.com>
2024-10-17 08:25:05 +0700

    2 - Data Annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace Bookstore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
 
         public int DisplayOrder { get; set; }
