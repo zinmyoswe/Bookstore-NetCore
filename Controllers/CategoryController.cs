9548379d2a1ace21cf8152d01d3f10d02d8de101
zinmyoswe <overmidnight12@gmail.com>
2024-10-17 09:19:09 +0700

    Create Seed , Controller and View of Category

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Bookstore.Data;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+
+        
+    }
+}
