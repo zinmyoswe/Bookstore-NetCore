a5f7dab9d0fa2150dd6988f5f041f8dd498b1eac
zinmyoswe <overmidnight12@gmail.com>
2024-10-18 15:56:43 +0700

    Delete Category and temp alert

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -35,6 +35,7 @@ namespace Bookstore.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -71,9 +72,48 @@ namespace Bookstore.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category Updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(x=>x.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(x=>x.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+            
+            
 
         }
 
